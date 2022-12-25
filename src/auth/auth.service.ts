db3df9ed4666fe6096d4e256ea1cdb39d9010fb7
Jacek Nowak <jaceknowak226@gmail.com>
2022-12-25 02:35:58 +0100

    Auth controller

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  signIn() {
+    {
+      msg: 'Signed in!';
+    }
+  }
+
+  signUp() {
+    {
+      msg: 'Signed up!';
+    }
+  }
+}
