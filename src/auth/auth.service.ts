cc6adbdaf5315547be1a1b43f15cb0b84a8a44f6
Jacek Nowak <jaceknowak226@gmail.com>
2022-12-28 21:49:12 +0100

    Sign in logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,24 +10,46 @@ export class AuthService {
   constructor(private prismaService: PrismaService) {}
 
   async signUp({ email, password }: AuthDto) {
+    // Generate the password hash
     const hash = await argon.hash(password);
+
     try {
+      // Save the new user in the db
       const user = await this.prismaService.user.create({
         data: { email, hash },
       });
       delete user.hash;
+
+      // Return the saved user
       return user;
     } catch (error) {
+      // Check if credentials are taken
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
           throw new ForbiddenException('Credentials taken');
         }
       }
+
+      // If not throw error
       throw error;
     }
   }
 
-  signIn() {
-    return { msg: 'Signed in!' };
+  async signIn({ email, password }: AuthDto) {
+    // Find the user by email
+    const user = await this.prismaService.user.findUnique({ where: { email } });
+
+    // If user does not exist throw exception
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    // Compare passwords
+    const pwMatches = await argon.verify(user.hash, password);
+
+    // If password is incorrect throw exception
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+
+    // Send back
+    delete user.hash;
+    return user;
   }
 }
